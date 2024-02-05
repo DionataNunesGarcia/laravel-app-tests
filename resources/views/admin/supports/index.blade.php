db5f8592c39067924b6407c1eadebfb2ccf9b6e0
DionataNunesGarcia <dionata.nunes.garcia@gmail.com>
2024-02-05 13:01:58 -0300

    add Support list, controller, model and migration

---
+++ resources/views/admin/supports/index.blade.php
@@ -0,0 +1,23 @@
+<h1>
+    Supports List
+</h1>
+<table>
+    <thead>
+        <th>Assunto</th>
+        <th>Status</th>
+        <th>Descrição</th>
+        <th>Ações</th>
+    </thead>
+    <tbody>
+        @foreach($supports as $support)
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->status }}</td>
+                <td>{{ $support->body }}</td>
+                <td>
+
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
