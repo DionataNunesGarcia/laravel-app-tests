93f2dc2331d8f988ff1d1acf604891af635c8f52
DionataNunesGarcia <dionata.nunes.garcia@gmail.com>
2024-02-11 12:22:26 -0300

    feat: create API routes, controller and resource.

---
+++ app/Http/Resources/SupportResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SupportResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'subject' => $this->subject,
+            'content' => $this->body,
+            'created_at' => Carbon::make($this->created_at)->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::make($this->updated_at)->format('Y-m-d H:i:s'),
+        ];
+    }
+}
